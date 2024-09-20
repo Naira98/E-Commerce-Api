fc956ac12e006295d364b562939ac4a68babfad7
Naira98 <nairamm99@gmail.com>
2024-09-20 17:47:50 +0300

    adding .env file

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,9 +5,7 @@ dotenv.config();
 const development = process.env.NODE_ENV === "development";
 const SERVER_PORT = Number(process.env.SERVER_PORT) || 1337;
 
-const MONGO_USER = process.env.MONGO_USER || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.xk4dvlj.mongodb.net/E-Commerce-Api`;
+const MONGO_URI = process.env.MONGO_URI || '';
 
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || "jwtacesssecret";
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "jwtrefreshsecret";
@@ -18,7 +16,7 @@ const config = {
     dev: development,
   },
   mongo: {
-    url: MONGO_URL,
+    url: MONGO_URI,
   },
   jwt: {
     accessSecret: JWT_ACCESS_SECRET,
